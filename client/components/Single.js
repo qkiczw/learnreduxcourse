f8412f355512e804332ab3aca882ba8348ae2969
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-09-17 20:10:44 +0200

    React Router added.

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render() {
+        return (
+            <div className='single-foto'>
+                I`m the single
+            </div>
+        )
+    }
+})
+
+export default Single;
